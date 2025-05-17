cbc33d18ba523a5b4545de1224a11a7c9565a61f
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-05-17 06:40:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import secrets
 from datetime import timedelta
 
 # إعدادات قاعدة البيانات
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("postgresql://_7833b8445553a4d6:_21dc1465df194c7bb8ec33ee1ae369@primary.mp31--vnbw5fmyn97t.addon.code.run:5432/_bd56d473bd07?sslmode=require")
 
 # إعدادات عامة
 SECRET_KEY = os.environ.get("SESSION_SECRET", "telegram_bot_security_key_fixed_2025")
